f7d3217dc8a22538ef126510a73e768d74ff19c4
speedy <jon@ownjoo.org>
2021-10-23 02:07:36 -0500

    initial commit

---
+++ src/main/java/org/ownjoo/springbootspa/SpringbootSpaApplication.java
@@ -0,0 +1,13 @@
+package org.ownjoo.springbootspa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ownjoo/springbootspa/SpringbootSpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ownjoo.springbootspa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
